a2d904694bfecf1f2d4ee98b51c224e49cd1e962
Yamiteru <yamiteru@icloud.com>
2025-10-05 15:29:38 +0200

    feat: add files config

--- config.py
+++ config.py
@@ -4,20 +4,55 @@ from dataclasses import dataclass
 @dataclass
 class Model:
     name: str
+    model_kwargs: dict
 
 
 @dataclass
 class Database:
     type: str
+    configuration: dict
+
+
+@dataclass
+class Files:
+    include: list[str]
+    exclude: list[str]
 
 
 @dataclass
 class Config:
     model: Model
     database: Database
+    files: Files
 
 
 default = Config(
-    Model("nomic-ai/CodeRankEmbed"),
-    Database("chromadb")
+    Model(
+        "nomic-ai/CodeRankEmbed",
+        {
+            "load_in_8bit": True
+        }
+    ),
+    Database(
+        "chromadb",
+        {
+            "hnsw": {
+                "space": "cosine",
+                "max_neighbors": 160,
+                "ef_search": 1000,
+                "ef_construction": 1000
+            }
+        }
+    ),
+    Files(
+        [
+            "ts",
+            "tsx"
+        ],
+        [
+            "test.ts",
+            "spec.ts",
+            "unit.ts"
+        ]
+    )
 )
